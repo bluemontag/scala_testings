9a0f58a7d13a1c60e8daccac3b76f9ab2650524e
Ignacio Gallego Sagastume <bluemontag@gmail.com>
2017-11-14 11:15:40 -0300

    format

--- src/parsers/JSONParser.scala
+++ src/parsers/JSONParser.scala
@@ -20,7 +20,13 @@ class JSON1 extends JavaTokenParsers {
   def obj: Parser[Map[String, Any]]   = "{" ~> repsep(member, ",") <~ "}" ^^ (Map() ++ _)
   def arr: Parser[List[Any]]          = "[" ~> repsep(value, ",") <~ "]"
   def member: Parser[(String, Any)] =  stringLiteral ~ ":" ~ value ^^ { case name ~ ":" ~ value => (name, value) }
-  def value: Parser[Any] = ( obj | arr | stringLiteral | floatingPointNumber ^^ (_.toDouble) | "null" ^^ (x => null) | "true" ^^ (x => true) | "false" ^^ (x => false))
+  def value: Parser[Any] = ( obj |
+                             arr | 
+                             stringLiteral |
+                             floatingPointNumber ^^ (_.toDouble) |
+                             "null" ^^ (x => null) |
+                             "true" ^^ (x => true) | "false" ^^ (x => false)
+                             )
 }
 
 object JSONParser extends JSON1 {
